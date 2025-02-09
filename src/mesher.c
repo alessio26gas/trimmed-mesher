bcf83326cd6829f0976d1b556ab6413e0cab44fe
alessio26gas <alessio26gas@libero.it>
2025-02-09 16:29:27 +0100

    added load_body function

--- src/mesher.c
+++ src/mesher.c
@@ -23,6 +23,37 @@ typedef struct {
     int pentaflag;
 } Element;
 
+int load_body(const char *filename, Point **body, int *n_points) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("An error has occured while opening the file");
+        return -1;
+    }
+
+    int count = 0;
+    double x, y;
+    while (fscanf(file, "%lf,%lf", &x, &y) == 2) {
+        count++;
+    }
+
+    *body = (Point *)malloc(count * sizeof(Point));
+    if (!*body) {
+        perror("An error has occurred");
+        fclose(file);
+        return -1;
+    }
+
+    rewind(file);
+    int i = 0;
+    while (fscanf(file, "%lf,%lf", &(*body)[i].x, &(*body)[i].y) == 2) {
+        i++;
+    }
+
+    *n_points = count;
+    fclose(file);
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
 
     if (argc < 4 || argc > 5) {
@@ -43,6 +74,12 @@ int main(int argc, char *argv[]) {
     if (rows < EPSILON || cols < EPSILON || cell_size < EPSILON) {
         printf("Unvalid values.\n");
         printf("Usage: %s <body.csv> <cellsize> <rows> [columns]\n", argv[0]);
+        return -1;
+    }
+
+    Point *body;
+    int n_points;
+    if (load_body(argv[1], &body, &n_points) != 0) {
         return -1;
     }
 
