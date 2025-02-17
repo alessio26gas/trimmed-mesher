54f0d95e92612de8e7a24c04217697682c02f28b
alessio26gas <alessio26gas@libero.it>
2025-02-17 23:32:18 +0100

    non-conformal coarsening

--- src/coarsening.c
+++ src/coarsening.c
@@ -1,12 +1,12 @@
 #include "coarsening.h"
 #include <math.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define EPSILON 1e-12
 
 void coarsening(Node **nodes, int *n_nodes, Element **elements, int *n_elements, Input input) {
-
-    bool conformal = false;
 
     int max_level = 0;
     for (int i = 0; i < 4; i++) {
@@ -14,6 +14,15 @@ void coarsening(Node **nodes, int *n_nodes, Element **elements, int *n_elements,
             max_level = input.coarsening_levels[i];
         }
     }
+    if (max_level == 0) return;
+
+    if (input.conformal_coarsening) {
+        printf("Conformal coarsening is not supported yet.\n");
+        return;
+    }
+
+    (*nodes) = realloc((*nodes), max_level * 2 * (*n_nodes) * sizeof(Node));
+    (*elements) = realloc((*elements), max_level * 2 * (*n_elements) * sizeof(Element));
 
     double xL = input.center.x - input.cell_size * input.cols / 2;
     double xH = input.center.x + input.cell_size * input.cols / 2;
@@ -30,9 +39,10 @@ void coarsening(Node **nodes, int *n_nodes, Element **elements, int *n_elements,
 
             int K = (*n_nodes);
 
-            int n = round(((side % 2) * (yH - yL) + ((side + 1) % 2) * (xH - xL)) / cell_size);
+            int n = round(((side % 2) * (yH - yL) + ((side + 1) % 2) * (xH - xL)) / cell_size)
+                    + input.coarsening_cells[(side + 1) % 4] * (input.coarsening_levels[(side + 1) % 4] >= l);
 
-            for (int j = conformal; j <= input.coarsening_cells[side] + conformal; j++) {
+            for (int j = input.conformal_coarsening; j <= input.coarsening_cells[side] + input.conformal_coarsening; j++) {
                 for (int i = 0; i <= n; i++) {
                     Point p;
                     if (side==0) p = (Point){xL + i * cell_size, yL - j * cell_size};
@@ -55,43 +65,17 @@ void coarsening(Node **nodes, int *n_nodes, Element **elements, int *n_elements,
                     (*n_elements)++;
                 }
             }
-
-            if (conformal) {
-                // TODO: Identify Boundary Nodes
-
-                // TODO: Create Triangular Elements
-            }
         }
 
-        double yLold = yL;
-        double xHold = xH;
-        double yHold = yH;
-        double xLold = xL;
-        yL -= (input.coarsening_cells[0] + conformal) * cell_size * (input.coarsening_levels[0] >= l);
-        xH += (input.coarsening_cells[1] + conformal) * cell_size * (input.coarsening_levels[1] >= l);
-        yH += (input.coarsening_cells[2] + conformal) * cell_size * (input.coarsening_levels[2] >= l);
-        xL -= (input.coarsening_cells[3] + conformal) * cell_size * (input.coarsening_levels[3] >= l);
+        yL -= (input.coarsening_cells[0] + input.conformal_coarsening) * cell_size * (input.coarsening_levels[0] >= l);
+        xH += (input.coarsening_cells[1] + input.conformal_coarsening) * cell_size * (input.coarsening_levels[1] >= l);
+        yH += (input.coarsening_cells[2] + input.conformal_coarsening) * cell_size * (input.coarsening_levels[2] >= l);
+        xL -= (input.coarsening_cells[3] + input.conformal_coarsening) * cell_size * (input.coarsening_levels[3] >= l);
 
-        if (yL < yLold - EPSILON) {
-            if (xL < xLold - EPSILON) {
-                // nodes
-
-                // elements
-            }
-            if (xH > xHold + EPSILON) {
-                // corner in basso a destra
+        if (input.fast_coarsening) {
+            for (int i = 0; i < 4; i++) {
+                input.coarsening_cells[i] = input.coarsening_cells[i] / 2;
             }
         }
-        if (yH > yHold + EPSILON) {
-            if (xL < xLold - EPSILON) {
-                // corner in alto a sinistra
-            }
-            if (xH > xHold + EPSILON) {
-                // corner in alto a destra
-            }
-        }
-
-        for (int i = 0; i < 4; i++)
-            input.coarsening_cells[i] /= 2;
     }
 }
