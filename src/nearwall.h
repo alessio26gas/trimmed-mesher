e707d620f7c6109d5dbabcf69b64ebe1f3959b3f
alessio26gas <alessio26gas@libero.it>
2025-03-13 09:16:45 +0100

    shared nearest_point function

--- src/nearwall.h
+++ src/nearwall.h
@@ -16,6 +16,7 @@ typedef struct {
 
 void compute_nwl(NearWallLayer *nwl);
 bool compute_offset(Point **offset, int *n_offset, Point *body, int n_body, double nwl_distance, double cell_size);
+Point nearest_point(Point p, Point *body, int n_points, double nwl_distance);
 int get_offset_nodes(int **offset_nodes, int *n_offset_nodes, Node *nodes, int n_nodes, double cell_size, double X0, double Y0, int rows, int cols);
 void extrude_near_wall_cells(
     Element **elements, int *n_elements,
