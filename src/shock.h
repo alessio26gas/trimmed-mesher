83dad8d23c790a4f79f02c2a158ca32e4a4bd8d0
alessio26gas <alessio26gas@libero.it>
2025-03-18 15:43:58 +0100

    shock functions definition

--- src/shock.h
+++ src/shock.h
@@ -13,5 +13,14 @@ typedef struct {
 } NearShockLayer;
 
 void compute_nsl(NearShockLayer *nsl);
+bool compute_shock_offset(Point **offset, int *n_offset, Point *shock, int n_shock, double nsl_distance, double cell_size);
+void extrude_near_shock_cells(
+    Element **elements, int *n_elements,
+    Node **nodes, int *n_nodes,
+    Point *shock, int n_shock,
+    int *offset_nodes, int n_offset_nodes,
+    NearShockLayer nsl, int simmetry,
+    double X0, int cols, double cell_size
+);
 
 #endif
